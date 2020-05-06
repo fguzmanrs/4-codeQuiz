c4ef1663726d332193ba25830d5371a069c31cca
Tiresias <fguzmanrs@gmail.com>
2020-05-05 21:11:01 -0700

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,14 +1,9 @@
 //quiz questions in an array of objects
 var questions = [
     {
-        title: "Commonly used data types DO NOT include:",
-        choices: ["strings", "booleans", "alerts", "numbers"],
-        answer: "alerts"
-    },
-    {
-        title: "The condition in an if / else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "parentheses"
+        title: "A useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["console.log", "react", "JavaScript", "terminal/bash"],
+        answer: "console.log"
     },
     {
         title: "Arrays in JavaScript can be used to store:",
@@ -16,13 +11,18 @@ var questions = [
         answer: "all of the above"
     },
     {
-        title: "String values must be enclosed within ____ when being assigned to variables.",
-        choices: ["commas", "curly brackets", "quotes", "parentheses"],
+        title: "String values must be enclosed within ____ when assigned to variables.",
+        choices: ["commas", "curly brackets", "parentheses", "quotes"],
         answer: "quotes"
     },
     {
-        title: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
-        answer: "console.log"
+        title: "When should you use GET as the method for submitting your form?",
+        choices: ["sensitive data", "search results people may want to bookmark", "order form", "large form with lots of data"],
+        answer: "search results people may want to bookmark"
+    },
+    {
+        title: "Which attribute tells the browser where to go when a hyperlink is clicked?",
+        choices: ["src", "url", "link", "href"],
+        answer: "href"
     }
 ];
